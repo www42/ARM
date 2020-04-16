4261952be25bb17bbec9f66a7f7f03a80d65b37b
www42 <thomas.jaekel@gfn.de>
2020-04-16 21:54:19 +0200

    ps1

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -1,2 +1,13 @@
-$PSVersionTable
-Get-Module -ListAvailable -Name Az
+# Login to Azure
+# --------------
+Connect-AzAccount
+Get-AzContext | fl
+
+
+$Location = "westeurope"
+$RgName = "RG-PS61"
+
+New-AzResourceGroup -Name $RgName -Location $Location
+
+$Template = "https://raw.githubusercontent.com/www42/arm/master/templates/ps-61.json"
+New-AzResourceGroupDeployment -ResourceGroupName $RgName -TemplateUri $Template
