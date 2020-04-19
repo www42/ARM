9516a1b6f1f4761892f66ed3613c878553cd69c0
www42 <thomas.jaekel@gfn.de>
2020-04-19 23:03:11 +0200

    aacount

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -5,9 +5,19 @@ Get-AzContext | fl
 
 
 $location = "westeurope"
-$rgName = "RG-PS61"
+$rgName = "PS61-RG"
 
 New-AzResourceGroup -Name $rgName -Location $location
+Get-AzResourceGroup 
 
 $template = "https://raw.githubusercontent.com/www42/arm/master/templates/ps-61.json"
 New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateUri $template
+
+# DSC
+# ---
+Get-AzAutomationAccount 
+$automationAccountName = "PS61-Automation"
+
+Get-AzAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $automationAccountName
+(Get-AzAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $automationAccountName).Endpoint
+(Get-AzAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $automationAccountName).PrimaryKey
