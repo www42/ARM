9ec25aceb5c45fd36974429dafceeccd14a8c89c
www42 <thomas.jaekel@gfn.de>
2020-09-24 11:28:29 +0200

    DCConfig example1

--- dscConfigs/DomainControllerConfig.ps1
+++ dscConfigs/DomainControllerConfig.ps1
@@ -1,1 +1,41 @@
-Configuration DomainControllerConfig {}
+Configuration ADDomain_NewForest_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain 'contoso.com'
+        {
+            DomainName                    = 'contoso.com'
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
